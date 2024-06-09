cb77f1622b9a1c88fe3af7e1c410db5640a0f748
Rajesh Chauhan <89766962+rajeshchauhan1426@users.noreply.github.com>
2024-06-09 17:25:18 +0530

    tailwind added

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <div>
+      <h1>hello</h1>
+    </div>
   )
 }
 
