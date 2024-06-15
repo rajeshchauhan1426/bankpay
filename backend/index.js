9ef2d21165f89f8c92bc6ac015d4d27f5584c62e
rajeshchauhan1426 <rc810609@gmail>
2024-06-15 16:56:17 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 // backend/index.js
 const express = require("express");
 const rootRouter = require("./api/index")
-
+const cors = require("cors");
 const app = express();
 
 app.use("/api/v1", rootRouter);
+
+
+m
