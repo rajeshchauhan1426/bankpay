74dbd7c96f7ca9c3a1128e0553d95ac888e89f12
Rajesh Chauhan <89766962+rajeshchauhan1426@users.noreply.github.com>
2024-06-16 11:35:45 +0530

    pc

---
+++ backend/api/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const router = root.Router();
+
+module.exports = router;
+
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-const express = require("express")
+// backend/index.js
+const express = require("express");
+const rootRouter = require("./api/index")
+const cors = require("cors");
+const app = express();
+
+app.use("/api/v1", rootRouter);
+
+
+app.listen(3000);
+
