7b7d8401c115b7d05791ecce4cb72cbfdffaca82
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-06-16 16:38:52 +0530

    mac

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const rootRouter = require("./api/index")
 const cors = require("cors");
+const router = require("./api/index");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/v1", rootRouter);
 
+module.exports = router;
 
+app.listen(3000)
