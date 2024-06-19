c2b6eb01122ee4ddd2e76f4b1e613876d170b45d
Rajesh Chauhan <89766962+rajeshchauhan1426@users.noreply.github.com>
2024-06-19 15:33:02 +0530

    a

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,11 @@
 // backend/routes/index.js
 const express = require('express');
-const userRouter = require("./user");
+const userRouter = require("./User");
 const accountRouter = require("./account")
 
 const router = express.Router();
 
-router.use("/User", userRouter)
+router.use("/user", userRouter)
 router.use("/account", accountRouter)
 
 module.exports = router;
