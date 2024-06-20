e1fddfd6730417d598e8e08a9249d6d702378a09
Rajesh Chauhan <89766962+rajeshchauhan1426@users.noreply.github.com>
2024-06-20 20:01:43 +0530

    frontend

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
