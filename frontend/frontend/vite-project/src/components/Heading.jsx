de1cc7d982e3c5fe7a4490a42d3343d47f092f31
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-07-15 20:16:09 +0530

    edited siguup

--- frontend/frontend/vite-project/src/components/Heading.jsx
+++ frontend/frontend/vite-project/src/components/Heading.jsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Heading({label}) {
-    return <div className="font-bold text-4xl pt-6">
-      {label}
-    </div>
+export default function Heading({ label }) {
+    return (
+        <div className="font-bold text-4xl pt-6">
+            {label}
+        </div>
+    );
 }
+
+Heading.propTypes = {
+    label: PropTypes.string.isRequired,
+};
