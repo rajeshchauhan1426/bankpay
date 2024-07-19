0842a3d737746744167f3d1c03134ed56bec30ec
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-07-19 10:06:07 +0530

    u

--- frontend/frontend/vite-project/src/pages/Signup.jsx
+++ frontend/frontend/vite-project/src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import BottomWarning from '/src/components/Bottomwarning.jsx';
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
+import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 import axios from "axios";
