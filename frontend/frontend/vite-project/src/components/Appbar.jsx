f63a885945644f122b871c007b34f66ce308fb62
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-08-04 18:25:34 +0530

    update

--- frontend/frontend/vite-project/src/components/Appbar.jsx
+++ frontend/frontend/vite-project/src/components/Appbar.jsx
@@ -9,7 +9,7 @@ export const Appbar = () => {
                 Hello
             </div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center h-full text-xl">
+                <div className="flex flex-col justify-center h-full text-xl text-xl">
                     U
                 </div>
             </div>
